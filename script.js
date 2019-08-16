7acefdfd26bd2fe50f7c34f052a4c3a8ba1f56b9
KiritoKyun <tylersashimi@gmail.com>
2019-08-16 09:08:53 -0700

    Updated for new schedule

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function t2min(x){
 	return (parseInt(x.slice(0,2),10)*60+parseInt(x.slice(3),10))
 }
-var latetimer; var updatetimer; var endtime; var remaining;
+var updatetimer; var endtime; var remaining;
 function check(){
 	if (typeof remaining == 'undefined'){
 		clear()
 	}
-}
-function setlatestarttimer(){
-	setInterval(latestart,1000)
-	clearInterval(updatetimer)
-	document.getElementById('late').innerHTML = ''
 }
 function definetime(){
 	window.today = new Date()
@@ -39,13 +34,17 @@ function final(timearray){
 }
 function update(){
 	definetime()
-	if ([1,3,5].includes(day)){
+	if ([1,5].includes(day)){
 		var normaltimes = ['06:45','07:37','08:41','09:50','10:54','11:09','12:10','13:14','13:44','14:45']
 		final(normaltimes)
 	}
 	else if ([2,4].includes(day)){
-		var tutorialtimes = ['06:45','07:37','08:41','09:50','10:54','11:09','12:10','13:14','13:44','14:45']
+		var tutorialtimes = ['06:45','07:37','08:35','09:37','10:35','10:50','11:25','12:23','13:21','13:51','14:45']
 		final(tutorialtimes)
+	}
+	else if (day == 3){
+		var latestarttimes = ['8:30','09:09','09:57','10:50','11:05','11:50','12:38','13:26','14:00','14:45']
+		final(latestarttimes)
 	}
 	check()
 }
@@ -54,14 +53,5 @@ function clear(){
 	remaining = 'Not in school'
 	commitchanges()
 }
-function latestart(){
-	definetime()
-	var latestarttimes = ['8:30','09:09','09:57','10:50','11:05','11:50','12:38','13:26','14:00','14:45']
-	final(latestarttimes)
-	check()
-}
 update()
 var updatetimer = setInterval(update,1000)
-if (day == 3){
-	document.getElementById('late').innerHTML = 'Click if late start'
-}
